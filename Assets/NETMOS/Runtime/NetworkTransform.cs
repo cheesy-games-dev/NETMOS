0d3509d91a8905d61b6ad70d23e752f7ce49cb94
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-06-17 14:28:17 +0100

    updated network transform and removed ios

--- Assets/NETMOS/Runtime/NetworkTransform.cs
+++ Assets/NETMOS/Runtime/NetworkTransform.cs
@@ -7,13 +7,20 @@ namespace Netmos
     {
         private Rigidbody rb;
         private ArticulationBody ab;
+        private bool wasImmovable;
 
         private void Start() {
             rb = GetComponent<Rigidbody>();
             ab = GetComponent<ArticulationBody>();
+            if (rb)
+                wasImmovable = rb.isKinematic;
+            else if(ab)
+                wasImmovable = ab.immovable;
         }
 
         private void Update() {
+            if (wasImmovable)
+                return;
             if (rb)
                 rb.isKinematic = !authority;
             else if(ab)
